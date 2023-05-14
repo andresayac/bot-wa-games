e7d2ee0d38d8f34b38890d27630b9efa9586335a
Andres Aya <andresayac@gmail.com>
2023-05-14 10:12:58 -0500

    change 0 to return menu in menu games

--- flows/menu/flowGames.js
+++ flows/menu/flowGames.js
@@ -9,17 +9,17 @@ const flowGames = addKeyword(['juegos', 'uno', '1'])
             'Lista de Juegos:',
             ' *(1)* - Akinator',
             ' *(2)* - Hangman (Ahorcado)',
-            ' *(3)* - Volver al menu principal',
+            ' *(0)* - Volver al menu principal',
         ],
         { capture: true },
         async (ctx, { fallBack, flowDynamic, gotoFlow }) => {
-            if (![1, 2, 3].includes(parseInt(ctx.body.toLowerCase().trim()))) {
+            if (![1, 2, 0].includes(parseInt(ctx.body.toLowerCase().trim()))) {
                 await flowDynamic(['Opcion no valida, por favor seleccione una opcion valida.'])
                 await fallBack()
                 return
             }
 
-            if (ctx.body.toLowerCase().trim() === '3') {
+            if (ctx.body.toLowerCase().trim() === '0') {
                 const flowMain = require('../flowMain')
                 await gotoFlow(flowMain)
                 return
