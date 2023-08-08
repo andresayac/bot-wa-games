14d453f7f0dfe1b9434882eb9a439b5d6d933747
Andres Aya <andresayac@gmail.com>
2023-08-07 20:47:42 -0500

    update add pairing code

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const JsonFileAdapter = require('@bot-whatsapp/database/json')
 
-require('dotenv').config()
+const dotenv = require('dotenv-safe')
+dotenv.config()
 
 const flowMain = require('./flows/flowMain')
 
 const main = async () => {
+    
+    const isPairingCode = process.env.WA_PAIRING_CODE.toLowerCase() === 'true' ?? false
+
     const adapterDB = new JsonFileAdapter()
     const adapterFlow = createFlow([flowMain])
-    const adapterProvider = createProvider(BaileysProvider)
+    const adapterProvider = createProvider(BaileysProvider, { usePairingCode: isPairingCode, phoneNumber: process.env.WA_PHONE_NUMBER ?? '' })
 
     createBot({
         flow: adapterFlow,
@@ -19,9 +23,11 @@ const main = async () => {
         database: adapterDB,
     })
 
-    QRPortalWeb({
-        port: process.env.PORTAL_PORT || 3000
-    })
+    if (!isPairingCode) {
+        QRPortalWeb({
+            port: process.env.PORTAL_PORT || 3000
+        })
+    }
 }
 
 main()
